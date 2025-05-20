983b113c8ed4700960b07006a475c6be50f4abc0
DoGiaHuy <dogiahuy1000@gmail.com>
2025-05-20 13:40:33 +0700

    fix deploy

--- src/socket.js
+++ src/socket.js
@@ -1,5 +1,8 @@
 import { io } from "socket.io-client";
 
-export const socket = io("http://localhost:3000", {
-  transports: ["websocket"],
-});
+export const socket = io(
+  import.meta.env.VITE_SOCKET_URL || "http://localhost:3000",
+  {
+    transports: ["websocket"],
+  }
+);
