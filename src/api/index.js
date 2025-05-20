f93bef018903496f0c5d4522c28b54c7c141591f
DoGiaHuy <dogiahuy1000@gmail.com>
2025-05-20 14:24:08 +0700

    update socket

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { toast } from "react-toastify";
 
-const baseURL = import.meta.env.VITE_API_URL || "http://localhost:3000";
+const baseURL = import.meta.env.VITE_API_URL;
 
 export const api = axios.create({
   baseURL,
